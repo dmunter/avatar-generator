b855309fd0f2e449d22c2ab69186df86c5f96568
Drew <dmunter27@gmail.com>
2023-03-17 17:03:18 -0500

    changed key

--- src/pages/api/stripe/checkout_sessions.js
+++ src/pages/api/stripe/checkout_sessions.js
@@ -1,5 +1,5 @@
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_PROD);
 
 export default async function handler(req, res) {
 
